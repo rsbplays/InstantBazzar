272f09f356d512819b1a3b18d90854726a826bb4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 17:16:34 +0100

    deez

--- src/main/java/generalrs/instantbazzar/BasicEvent.java
+++ src/main/java/generalrs/instantbazzar/BasicEvent.java
@@ -1,16 +1,36 @@
 package generalrs.instantbazzar;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityCategory;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
 
 public class BasicEvent implements Listener {
 
+    @EventHandler
     public void onJoin(PlayerJoinEvent event){
+        InstantBazzar.sayHello.sayHelloToPlayer(event.getPlayer());
 
+        BukkitTask taskid = Bukkit.getScheduler().runTaskTimer(InstantBazzar.instance, new Runnable() {
+            @Override
+            public void run() {
+                event.getPlayer().getWorld().dropItem(event.getPlayer().getLocation().add(0,3,0), new ItemStack(Material.DIAMOND));
 
+            }
 
-
+        },1,1);
+        Bukkit.getScheduler().runTaskLater(InstantBazzar.instance, new Runnable() {
+            @Override
+            public void run() {
+                taskid.cancel();
+            }
+        },100);
     }
 }
--- src/main/java/generalrs/instantbazzar/InstantBazzar.java
+++ src/main/java/generalrs/instantbazzar/InstantBazzar.java
@@ -1,14 +1,21 @@
 package generalrs.instantbazzar;
 
+import generalrs.instantbazzar.Commands.SayHello;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InstantBazzar extends JavaPlugin {
+    public static SayHello sayHello = new SayHello(500);
+    public static InstantBazzar instance;
 
     @Override
     public void onEnable() {
+        instance = this;
         // Plugin startup logic
         System.out.println("test 2");
         System.out.println("test 1");
+
+        System.out.println("jh;lfsadh lfkdaslhnbyckluafdsbitckjasdihtd,fcrklyjdzsmgulkvcgnjyasdfuiojhfrydlkjzjycrfadlzyjgfclikzjynflkdszgyfvgkasjyngflvtadsjyoifejadsc8oloc;fu");
+        getServer().getPluginManager().registerEvents(new BasicEvent(),this);
     }
 
     @Override
