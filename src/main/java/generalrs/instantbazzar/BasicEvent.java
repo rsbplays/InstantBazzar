d3c73a6014c1cb0b4396711cda7c678c1d565276
steven k <bobbyjellosteve@gmail.com>
2022-08-21 15:39:24 +0000

    sync test

--- src/main/java/generalrs/instantbazzar/BasicEvent.java
+++ src/main/java/generalrs/instantbazzar/BasicEvent.java
@@ -1,11 +1,20 @@
 package generalrs.instantbazzar;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class BasicEvent implements Listener {
 
     public void onJoin(PlayerJoinEvent event){
+
+        public void sayHelloToPlayer(Player p){
+            for (int times = 0;times<num;times++){
+                Bukkit.getLogger().info("Hello");
+            }
+        }
+
 
     }
 }
