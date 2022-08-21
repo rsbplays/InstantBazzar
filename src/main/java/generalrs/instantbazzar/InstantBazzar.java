5346efc65a9957cb316c7c60268cf98947f374de
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 15:44:05 +0100

    sync test

--- src/main/java/generalrs/instantbazzar/InstantBazzar.java
+++ src/main/java/generalrs/instantbazzar/InstantBazzar.java
@@ -7,7 +7,7 @@ public final class InstantBazzar extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        
+        System.out.println("test 1");
     }
 
     @Override
