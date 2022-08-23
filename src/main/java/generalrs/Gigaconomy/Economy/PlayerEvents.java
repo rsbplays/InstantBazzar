c0beecb8a2d79987ce1fe00b84572fdf0eef2d56
steven k <bobbyjellosteve@gmail.com>
2022-08-23 13:20:58 +0000

    System to check if player has account, and if they don't make them an account.

--- src/main/java/generalrs/Gigaconomy/Economy/PlayerEvents.java
+++ src/main/java/generalrs/Gigaconomy/Economy/PlayerEvents.java
@@ -1,5 +1,9 @@
 package generalrs.Gigaconomy.Economy;
 
+import generalrs.Gigaconomy.Data.YamlDataSaver;
+import generalrs.Gigaconomy.Economy.Data.BankAccount;
+import generalrs.Gigaconomy.Gigaconomy;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,6 +11,14 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerEvents implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
-        
+        if (Gigaconomy.dataHandler.playerHasAccount(e.getPlayer()));
+        {
+
+            Gigaconomy.dataHandler.createAccountForPlayer(e.getPlayer());
+            e.getPlayer().sendMessage("Your Account has been created");
+        }else{
+            e.setJoinMessage( e.getPlayer().getDisplayName()+"Welcome to the server");
+        }
+
     }
 }
